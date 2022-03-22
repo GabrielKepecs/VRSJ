f262f926ca5e70183c87e8a0217b362544f87250
Gezuiz <Lucas.aran1306@gmail.com>
2022-03-22 20:18:32 -0300

    Correção da rotação celular

--- Assets/Codes/EventTrigger.cs
+++ Assets/Codes/EventTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EventTrigger : MonoBehaviour
 {
@@ -48,7 +49,7 @@ public class EventTrigger : MonoBehaviour
 
             if (EndGame)
             {
-                Application.Quit();
+                SceneManager.LoadScene("Menu");
                 Debug.Log("Acabou");
             }
         }
